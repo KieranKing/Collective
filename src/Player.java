dc299c1a154f26334c14484680b67e10c6261295
RalphMatzner <ralphmatzner@yahoo.co.uk>
2015-04-11 16:26:12 +0100

    Added lexer and parser to Player class, integer codes for enums and java doc added.

--- src/Player.java
+++ src/Player.java
@@ -4,19 +4,13 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.*;// check what is really needed.
-import java.lang.*;// check what is really needed.
+import java.util.*;
+import java.lang.*;
+
 
 /**
- * This class represnts a single player.
- *
- * @author Kieran
- */
-/**
- * The Player class creates a Player object with a unique id, a name, an ant
- * brain, and a colour by which the player identifies his/her ants. The player
- * also keeps track of the amount of food his/her ants gather, how many ants
- * still are alive.
+ * The Player class creates a Player object with a unique id, a name, a list of ant
+ * brain instructions, and a colour by which the player identifies himself/herself in the match.
  *
  * @author Ralph
  */
@@ -27,67 +21,60 @@ public class Player {
 	private Colour colour;
 	private Instruction[] instruction;
 
+	/**
+	 * Represents the sensedir part of the antbrain CFG.
+	 *
+	 */
 	private enum sensedir {
 
 		Here, Ahead, LeftAhead, RightAhead
 	};
 
+	/**
+	 * Represents the cond part of the ant brain CFG.
+	 *
+	 */
 	private enum cond {
 
 		Friend, Foe, FriendWithFood, FoeWithFood, Food, Rock, Marker, FoeMarker, Home, FoeHome
 	};
 
+	/**
+	 * Represents the lr part of the ant brain CFG.
+	 *
+	 */
 	private enum lr {
 
 		Left, Right
 	};
 
+
 	/**
-	 * Kieran Construct this player.
-	 *
-	 * @param a unique id of this player
-	 * @param the name of this player
-	 * @param the path of the brain file
-	 */
-	/**
-	 * Ralph Generates a Player object with a name, an id, an ant brain and a
+	 * Generates a Player object with a name, an id, a list of instructin generated from the specified brain file and a
 	 * colour.
 	 *
 	 * @param id The id required to uniquely identify the player.
 	 * @param name The name of the player.
-	 * @param brainFilePath The path to the players ant brain.
-	 * @param colour The colour the player will use for his ants, red or black.
+	 * @param brainPath The path to the player's ant brain file.
 	 */
 	Player(int id, String name, String brainPath) throws LexicalException, SyntaxException, FileNotFoundException, IOException {
-		// Note: brainPath "null" should load a default brain for testing purposes
 		this.id = id;
 		this.name = name;
-		this.colour = null;
-		loadBrain(brainPath);
+		colour = null;
+		instruction = loadBrain(brainPath);
 	}
 
 	/**
-	 * Kieran Get the unique id of this player.
-	 *
-	 * @return unique id
-	 */
-	/**
-	 * Ralph Gets the players unique id.
+	 * Gets the player's unique id.
 	 *
 	 * @return Returns the players unique id.
 	 */
 	public int getId() {
-		// Note: Add this to class diagram
 		return id;
 	}
 
 	/**
-	 * Kieran Get the name of this player.
-	 *
-	 * @return the name
-	 */
-	/**
-	 * Ralph Gets the name of the player.
+	 * Gets the player's name.
 	 *
 	 * @return Returns the player's name.
 	 */
@@ -95,13 +82,9 @@ public class Player {
 		return name;
 	}
 
+
 	/**
-	 * Kieran Get the colour of this player.
-	 *
-	 * @return the colour
-	 */
-	/**
-	 * Ralph Gets the colour that represents a player in a match.
+	 * Gets the colour that represents the player in a match.
 	 *
 	 * @return Returns the colour that represents the player in a match.
 	 */
@@ -109,13 +92,9 @@ public class Player {
 		return colour;
 	}
 
+
 	/**
-	 * Kieran Set the colour of this player.
-	 *
-	 * @return the colour
-	 */
-	/**
-	 * Ralph Sets the player's colour to a given colour.
+	 * Sets the player's colour to the given colour.
 	 *
 	 * @param colour The colour that the will be used to reperesent a player in
 	 * a match.
@@ -135,11 +114,12 @@ public class Player {
 	}
 
 	/**
-	 * Load an antbrain file..
+	 * Generates a list of instructions from the specified brain file and returns the list.
 	 *
-	 * @param the location of the antbrain file
+	 * @param The file path leading to the antbrain file.
+	 * @return Returns an array of Instruction objects.
 	 */
-	private void loadBrain(String brainPath) throws LexicalException, SyntaxException, FileNotFoundException, IOException {
+	private Instruction[] loadBrain(String brainPath) throws LexicalException, SyntaxException, FileNotFoundException, IOException {
 		File file = new File(brainPath);
 		FileReader fileReader = new FileReader(file);
 		BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -148,6 +128,8 @@ public class Player {
 		String str = bufferedReader.readLine();
 		int i = 0;
 		Character c;
+
+		// Lexing of the ant brain.
 		try {
 			while (str != null) {
 				c = str.charAt(i);
@@ -181,7 +163,7 @@ public class Player {
 					throw new LexicalException("Invalid token: " + c.toString());
 				}
 			}
-		}catch(IOException e){ //double check if this is right.
+		}catch(IOException e){
 			fileReader.close();
 		}
 
@@ -239,8 +221,9 @@ public class Player {
 			}
 		}
 
-		//Parsing Stage.
+		//Parsing of the antbrain.
 		int x = 0;
+		Instruction[] instructions = new Instruction[tokenlist.size()];
 		for (int z = 0; z < tokenList.size(); z++) {
 			Object token = tokenList.get(z);
 			if (tokenList.get(z) instanceof Action) {
@@ -258,47 +241,47 @@ public class Player {
 					} else {
 						param[3] = recurseCond(tokenList, z);
 					}
-					instruction[x] = new Instruction(Action.SENSE, param);
+					instructions[x] = new Instruction(Action.SENSE, param);
 				}
 				else if(token.equals(Action.MARK)){
 					int[] param1 = new int[2];
 					param1[0] = recurseInt(tokenList, z);
 					z++;
 					param1[1] = recurseInt(tokenList, z);
-					instruction[x] = new Instruction(Action.MARK, param1);
+					instructions[x] = new Instruction(Action.MARK, param1);
 				}
 				else if(token.equals(Action.UNMARK)){
 					int[] param2 = new int[2];
 					param2[0] = recurseInt(tokenList, z);
 					z++;
 					param2[1] = recurseInt(tokenList, z);
-					instruction[x] = new Instruction(Action.UNMARK, param2);
+					instructions[x] = new Instruction(Action.UNMARK, param2);
 				}
 				else if(token.equals(Action.PICKUP)){
 					int[] param3 = new int[2];
 					param3[0] = recurseInt(tokenList, z);
 					z++;
 					param3[1] = recurseInt(tokenList, z);
-					instruction[x] = new Instruction(Action.PICKUP, param3);
+					instructions[x] = new Instruction(Action.PICKUP, param3);
 				}
 				else if(token.equals(Action.DROP)){
 					int[] param4 = new int[1];
 					param4[0] = recurseInt(tokenList, z);
-					instruction[x] = new Instruction(Action.DROP, param4);
+					instructions[x] = new Instruction(Action.DROP, param4);
 				}
 				else if(token.equals(Action.TURN)){
 					int[] param5 = new int[2];
 					param5[0] = recurseLr(tokenList, z);
 					z++;
 					param5[1] = recurseInt(tokenList, z);
-					instruction[x] = new Instruction(Action.TURN, param5);
+					instructions[x] = new Instruction(Action.TURN, param5);
 				}
 				else if(token.equals(Action.MOVE)){
 					int[] param6 = new int[2];
 					param6[0] = recurseInt(tokenList, z);
 					z++;
 					param6[1] = recurseInt(tokenList, z);
-					instruction[x] = new Instruction(Action.MOVE, param6);
+					instructions[x] = new Instruction(Action.MOVE, param6);
 				}
 				else if(token.equals(Action.FLIP)){
 					int[] param7 = new int[3];
@@ -307,99 +290,129 @@ public class Player {
 					param7[1] = recurseInt(tokenList, z);
 					z++;
 					param7[2] = recurseInt(tokenList, z);
-					instruction[x] = new Instruction(Action.FLIP, param7);
+					instructions[x] = new Instruction(Action.FLIP, param7);
 				}
 				x++;
 
 			} else {
-				//throw error is not an Action.
+				//throw error if not an Action/ syntax is faulty.
 				throw new SyntaxException("Invalid token order: " + token.toString());
 			}
 		}
+
+		return instructions;
 	}
 
+	/**
+	 * Checks whether the specified token is an enum in the dir class, returns the required integer or throws a syntax error.
+	 *
+	 * @param tokens The token to be checked.
+	 * @param index The index of the token in tokens to be checked.
+	 * @return Returns an integer for the parameter of the instruction being processed or an error if invalid syntax.
+	 */
 	private int recurseSenseDir(ArrayList<Object> tokens, int index) throws SyntaxException {
 		int param = 0;
 		Object token = tokens.get(index);
 		if (token instanceof sensedir) {
 			if(token.equals(sensedir.Here)) {
-				param =;
+				param = 0;
 			}
 			else if(token.equals(sensedir.Ahead)){
-				param =;
+				param = 1;
 			}
 			else if(token.equals(sensedir.LeftAhead)){
-				param =;
+				param = 2;
 			}
 			else if(token.equals(sensedir.RightAhead)){
-				param =;
+				param = 3;
 			}
 		} else {
-			//throw error if parameters are faulty.
+			//throw error if syntax is faulty.
 			throw new SyntaxException("Invalid token order: " + token.toString());
 
 		}
 		return param;
 	}
 
+	/**
+	 * Checks whether the specified token is an enum in the cond class, returns the required integer or throws a syntax error.
+	 *
+	 * @param tokens The token to be checked.
+	 * @param index The index of the token in tokens to be checked.
+	 * @return Returns an integer for the parameter of the instruction being processed or an error if invalid syntax.
+	 */
 	private int recurseCond(ArrayList<Object> tokens, int index) throws SyntaxException {
 		int param = 0;
 		Object token = tokens.get(index);
 		if (token instanceof cond) {
 			if(token.equals(cond.Friend)){
-				param =; //fill these in.
+				param = 0;
 			}
 			else if(token.equals(cond.Foe)){
-				param =;
+				param = 1;
 			}
 			else if(token.equals(cond.FriendWithFood)){
-				param =;
+				param = 2;
 			}
 			else if(token.equals(cond.FoeWithFood)){
-				param =;
+				param = 3;
 			}
 			else if(token.equals(cond.Food)){
-				param =;
+				param = 4;
 			}
 			else if(token.equals(cond.Rock)){
-				param =;
+				param = 5;
 			}
 			else if(token.equals(cond.Marker)){
 				param = recurseInt(tokens, index+1);
 			}
 			else if(token.equals(cond.FoeMarker)){
-				param =;
+				param = 7;
 			}
 			else if(token.equals(cond.Home)){
-				param =;
+				param = 8;
 			}
 			else if(token.equals(cond.FoeHome)){
-				param =;
+				param = 9;
 			}
 		}else{
-			//throw error
+			//throw error if syntax is faulty.
 			throw new SyntaxException("Invalid token order: " + token.toString());
 		}
 		return param;
 	}
 
+	/**
+	 * Checks whether the specified token is an enum in the lr class, returns the required integer or throws a syntax error.
+	 *
+	 * @param tokens The token to be checked.
+	 * @param index The index of the token in tokens to be checked.
+	 * @return Returns an integer for the parameter of the instruction being processed or an error if invalid syntax.
+	 */
 	private int recurseLr(ArrayList<Object> tokens, int index) throws SyntaxException {
 		int param = 0;
 		Object token = tokens.get(index);
 		if (token instanceof lr) {
 			if(token.equals(lr.Left)){
-				param = ;
+				param = 0;
 			}
 			else if(token.equals(lr.Right)){
-				param =;
+				param = 1;
 			}
 		} else {
-			//throw error.
+			//throw error if syntax is faulty.
 			throw new SyntaxException("Invalid token order: " + token.toString());
 		}
 		return param;
 	}
 
+	/**
+	 * Checks whether the specified token is an integer, returns integer else throws syntax error.
+	 *
+	 * @param tokens The list of tokens from which a specific token is to be checked.
+	 * @param index The index of the token in tokens representing an integer.
+	 * @return Returns an integer for the parameter of the instruction being processed or an error if invalid syntax.
+	 */
 	private int recurseInt(ArrayList<Object> tokens, int index) throws SyntaxException {
 		int param = 0;
 		Object token = tokens.get(index);
@@ -407,12 +420,18 @@ public class Player {
 			param = (int) token;
 		}
 		else{
-			//throw error
+			//throw error if syntax is faulty.
 			throw new SyntaxException("Invalid token order: " + token.toString());
 		}
 		return param;
 	}
 
+	/**
+	 * Checks whether the specified character is part of a keyword.
+	 *
+	 * @param c The character to be check whether it is part of a keyword or not.
+	 * @return Returns true if the specified character is part of a keyword else false.
+	 */
 	private boolean isKeyWord(char c) {
 		String s = "";
 		s += c;
@@ -423,6 +442,12 @@ public class Player {
 		}
 	}
 
+	/**
+	 * Checks whether the specified character is an integer.
+	 *
+	 * @param c The character to be check whether it is an integer or not.
+	 * @return Returns true if the specified character is an integer else false.
+	 */
 	private boolean isInt(char c) {
 		String s = "";
 		s += c;
@@ -431,19 +456,5 @@ public class Player {
 		} else {
 			return false;
 		}
-	}
-}
-
-class LexicalException extends Exception {
-
-	LexicalException(String string) {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-	}
-}
-
-class SyntaxException extends Exception {
-
-	SyntaxException(String string) {
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 }
