bbe608bd9627418b0acbc77b4a0430e040d17a08
RalphMatzner <ralphmatzner@yahoo.co.uk>
2015-04-11 16:31:54 +0100

    Added lexer and parser to Player class, integer codes for enums and java doc added.

---
+++ src/LexicalException.java
@@ -0,0 +1,9 @@
+/*
+ * Exception for throwing lexical exceptions, for ant brain files or world files when undergoing lexical analysis.
+ */
+class LexicalException extends Exception {
+
+    LexicalException(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ src/SyntaxException.java
@@ -0,0 +1,6 @@
+class SyntaxException extends Exception {
+
+    SyntaxException(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
